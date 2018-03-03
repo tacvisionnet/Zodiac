ed45af801906c1625d4ce6bbe3759770e54e3ea5
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-02 23:00:04 -0700

    Add files via upload

---
+++ characters.php
@@ -0,0 +1,28 @@
+<div align="left" hidden style="background-color: rgba(222, 222, 222, 1); color: #1b8590; border-radius: 5px; padding: 5px; position: relative; font-family: Georgia; font-size: 9pt; width: 450px; max-height: 500px; z-index: 1; border: solid white 1px; box-shadow: rgba(0, 0, 0, .8) 0 2px 8px; margin: auto; font-size: 14pt; overflow-y: auto; top: 150px;">
+<table align="right" style="font-size: 10pt;">
+              <tr>
+                <td>Color Key
+                  <br/><span class="bug">• Bug Fix</span>
+                  <br/><span class="addition">• Addition</span>
+                  <br/><span class="change">• Change</span>
+                  <br/><span class="newpage">• New Page</span>
+                  <br/><span class="misc">• Miscellaneous</span>
+                </td>
+              </tr>
+            </table>
+			
+			<div>
+              <fieldset style="border: none;" class="majorpoints">
+                <legend class="majorpointslegend"><b style="background: #d0d0d0; font-size: 14pt; text-shadow: rgba(255, 255, 255, .8) 0 1px 2px;">&nbsp; 鼷 &nbsp;</b></legend>
+                <div class="hiders" style="display:none">
+				
+				<b>2.28.72</b>
+                  <ul style="font-size: 10pt;">
+				  <li class="newpage"><b>Welcome!</b></li>
+                  </ul>
+				  
+				  </div>
+				  </fieldset>
+				  </div>
+
+</div>
