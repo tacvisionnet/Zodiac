d0548a21002b96b00260c58166aba87cb7e1ef6e
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-02 21:59:37 -0700

    Update articles.php

--- articles.php
+++ articles.php
@@ -3,34 +3,34 @@
     <h2 align="center" style="
     font-size: 24pt;
 ">授权证书</h2>
-	  <table style="margin: auto;padding: 0px 0px 20px 0px;width: 100%;">
-	  <tbody><tr>
-	  <td style="
-    text-align: right;
-    width: 50%;
-">
+	  <table style="margin: auto; padding: 0px 0px 20px 0px; width: 100%;">
+	  <tr>
+	  <td style="text-align: right; width: 50%;">
 	  密码 : &nbsp;
 	  </td>
-	  <td><input id="logopass" type="password" style="
-    width: 75px;
-">
+	  <td><input id="logopass" type="password" style="width: 75px;">
 	  </td>
 	  </tr>
 	  <tr>
 	  <td><br></td>
 	  </tr>
-	  <tr style="text-align: center;width: 100%;">
+	  <tr style="text-align: center; width: 100%;">
 	  
 	  <td align="right" id="logosend"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 发送 </span></td>
 	  <td align="left" id="logoclose"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 取消 </span></td>
 	  </tr>
-	  </tbody></table>
+	  </table>
 </div>
 
 <div hidden id="fail">
     <h2 id="fail2" align="center">
 	拒绝访问
 	</h2>
+	<table style="margin: auto; padding: 0px 0px 20px 0px; width: 100%;">
+	  <tr style="text-align: center; width: 100%;">
+	  <td align="center" id="failclose"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 取消 </span></td>
+	  </tr>
+	  </table>
 </div>
 
 <!-- Popup - End -->
@@ -48,9 +48,14 @@ $(document).ready(function() {
     document.getElementById('logosend').onclick = function() {
       if ($("#logopass").val() != "one") {
 		  $('#login').delay(50).fadeOut();
-		  $('#fail').delay(65).fadeIn();
-		  $('#fail').delay(1005).fadeOut();
-		  $('#login').delay(1405).fadeIn();
+		  $('#fail').delay(75).fadeIn();
+	  }
+    }
+	
+	document.getElementById('failclose').onclick = function() {
+      if ($("#logopass").val() != "one") {
+		  $('#fail').delay(50).fadeOut();
+		  $('#login').delay(75).fadeIn();
 	  }
     }
 </script>
