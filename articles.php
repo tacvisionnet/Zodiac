268ada4debb26a44f0f2fe73e73345a9416927b3
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-02 21:16:47 -0700

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,30 +1,37 @@
 <!-- Popup -->
 <div class="login">
-    <h2 align="center">授权证书</h2>
-	  <table style="margin: auto;padding: 20px;">
-	  <tr>
-	  <td colspan="2">
+    <h2 align="center" style="
+    font-size: 24pt;
+">授权证书</h2>
+	  <table style="margin: auto;padding: 0px 0px 20px 0px;width: 100%;">
+	  <tbody><tr>
+	  <td style="
+    text-align: right;
+    width: 50%;
+">
 	  密码 : 
 	  </td>
-	  <td colspan="2"><input id="logopass" type="password">
+	  <td><input id="logopass" type="password" style="
+    width: 75px;
+">
 	  </td>
 	  </tr>
 	  <tr>
 	  <td><br></td>
 	  </tr>
-	  <tr style="text-align: center;">
-	  <td colspan="2"></td>
-	  <td id="logosend"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 发送 </span></td>
-	  <td id="logoclose"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 取消 </span></td>
+	  <tr style="text-align: center;width: 100%;">
+	  
+	  <td align="right" id="logosend"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 发送 </span></td>
+	  <td align="left" id="logoclose"><span style="border: 1px solid silver; cursor: pointer;padding: 2px 10px;"> 取消 </span></td>
 	  </tr>
-	  </table>
+	  </tbody></table>
 </div>
 <!-- Popup - End -->
 
 <script>
 $(document).ready(function() {
     $('#logoclose').click(function(e) {
-    $('#logo1').delay(250).fadeOut();
+    $('#login').delay(250).fadeOut();
     });
     $('#logo').click(function(e) {
     $('#logo1').delay(250).fadeIn();
