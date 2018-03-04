1bd27e86151ec59d748fe338fd8296ef695a8889
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-03 23:02:59 -0700

    Add files via upload

---
+++ items.php
@@ -0,0 +1,93 @@
+<div id="itcontainer">
+
+
+<h1 style="font-family: 'Libre Barcode 128 Text', cursive; font-size: 92px; text-align: center; line-height: 12pt; font-weight: 400;">Items</h1>			
+		
+	
+	<!-- Date 1 -->
+	<div>
+              <fieldset style="border: none;" class="majorpoints">
+                <legend class="majorpointslegend"><b>&nbsp; Date 1 &nbsp;</b></legend>
+                <div class="hiders" style="display:none">
+				
+				<b>"itsion Name"</b>
+                  <ul style="font-size: 10pt;">
+				  <li class="newpage">Peter Piper picked a peck of pickled peppers.</li>
+				  <li class="bug">Waluigi something something.</li>
+				  <li class="change">Hello my baby, hello my honey. Hello my ragtime, summertime gal.</li>
+                  </ul>
+				  
+				  </div>
+				  </fieldset>
+				  </div>
+				  <!-- Date 1 - end -->
+				  
+				  <!-- Date 2 -->
+	<div>
+              <fieldset style="border: none;" class="majorpoints">
+                <legend class="majorpointslegend"><b>&nbsp; Date 2 &nbsp;</b></legend>
+                <div class="hiders" style="display:none">
+				
+				<b>"itsion Name"</b>
+                  <ul style="font-size: 10pt;">
+				  <li class="newpage">Peter Piper picked a peck of pickled peppers.</li>
+				  <li class="bug">Waluigi something something.</li>
+				  <li class="change">Hello my baby, hello my honey. Hello my ragtime, summertime gal.</li>
+                  </ul>
+				  
+				  </div>
+				  </fieldset>
+				  </div>
+				  <!-- Date 3 - end -->
+				  
+
+
+
+<script>
+$(function() {
+      $('.majorpoints').click(function() {
+        $(this).find('.hiders').toggle();
+      });
+	  
+});
+
+</script>
+
+<style>
+
+#itcontainer {
+    background-color: rgba(222, 222, 222, 0.17);
+    color: #5384ff;
+    border-radius: 5px;
+    padding: 5px;
+    position: relative;
+    font-family: Simsun;
+    width: 850px;
+    height: auto;
+    z-index: 1;
+    border: solid white 1px;
+    box-shadow: rgba(0, 0, 0, .8) 0 2px 8px;
+    margin: auto;
+    font-size: 14pt;
+    overflow-y: auto;
+    top: 50px;
+}
+
+.itparagraph {
+	width: 600px;
+	list-style: none;
+}
+
+.majorpointslegend {
+	    position: relative;
+    top: 0px;
+    left: -25px;
+    width: auto;
+    text-align: center;
+    list-style: none;
+    background: #0000004f;
+	font-size: 14pt;
+	color: #8edc39;
+}
+
+</style>
