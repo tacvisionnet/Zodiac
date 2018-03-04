798080068bbc43993c40ef632026567200651a97
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-03 22:39:17 -0700

    Update sessions.php

--- sessions.php
+++ sessions.php
@@ -1,258 +1,35 @@
 <div id="sescontainer">
 
-<!-- chimages -->
-<div hidden id="sesimg1a" class="imga">
-<div class="imgclose">X</div>
-<img id="sesimg1b" class="imgb" src="companylogo.png">
-</div>
-
-<div hidden id="sesimg2a" class="imga">
-<div class="imgclose">X</div>
-<img id="sesimg2b" class="imgb" src="companylogo.png">
-</div>
-
-<div hidden id="sesimg3a" class="imga">
-<div class="imgclose">X</div>
-<img id="sesimg3b" class="imgb" src="companylogo.png">
-</div>
-
-<div hidden id="sesimg4a" class="imga">
-<div class="imgclose">X</div>
-<img id="sesimg4b" class="imgb" src="companylogo.png">
-</div>
-<!-- chimages - end -->
 
 <h1 style="font-family: 'Libre Barcode 128 Text', cursive; font-size: 92px; text-align: center; line-height: 12pt; font-weight: 400;">Sessions</h1>			
-			
-			<div class="tabs2">
-    <ul class="tab2-links">
-      <li id="sesdate1"><a href="#sesdate1a">Character1</a></li>
-	  <li id="sesdate2"><a href="#sesdate2a">Character2</a></li>
-	  <li id="sesdate3"><a href="#sesdate3a">Character3</a></li>
-	  <li id="sesdate4"><a href="#sesdate4a">Character4</a></li>
-    </ul>
-	
-	<div class="tab-content">
+		
 	
 	<!-- Date 1 -->
-     <div id="sesdate1a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px;">
-				<tr>
-				<td>Race 1
-				</td>
-				<td>Class/Level
-				</td>
-				<td>Alignment
-				</td>
-				<td>Residence
-				</td>
-				</tr>
-				<tr>
-				<td>Occupation
-				</td>
-				<td>Date of Birth
-				</td>
-				<td>Nationality
-				</td>
-				<td>Gender
-				</td>
-				</tr>
-				<tr>
-				<td>Age
-				</td>
-				<td>Weight/Height
-				</td>
-				<td>Hair
-				</td>
-				<td>Eyes
-				</td>
-				</tr>
-				</table>
+	<div>
+              <fieldset style="border: none;" class="majorpoints">
+                <legend class="majorpointslegend"><b style="background: #d0d0d0; font-size: 14pt; text-shadow: rgba(255, 255, 255, .8) 0 1px 2px;">&nbsp; Date &nbsp;</b></legend>
+                <div class="hiders" style="display:none">
 				
-				<b style="font-size: 18pt;">Backstory</b>
+				<b>Patch 1.18.1</b>
                   <ul style="font-size: 10pt;">
-				  <li class="sesparagraph">
-				  <img id="sesimg1" class="img" src="companylogo.png">
-				  <b>It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.</b></li>
+				  <li class="newpage"><b>Added Citizens CC page!</b></li>
+				  <li class="bug">Corrected ACT and DTE pages asking for different jacks during DSL troubleshooting.</li>
+				  <li class="change">Updated CM lookup link on 3r.</li>
                   </ul>
-				  </div>
-				  <!-- Date 1 - End -->
 				  
-				  
-				  <!-- Date 2 -->				  
-				  <div id="sesdate2a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px;">
-				<tr>
-				<td>Race 2
-				</td>
-				<td>Class/Level
-				</td>
-				<td>Alignment
-				</td>
-				<td>Residence
-				</td>
-				</tr>
-				<tr>
-				<td>Occupation
-				</td>
-				<td>Date of Birth
-				</td>
-				<td>Nationality
-				</td>
-				<td>Gender
-				</td>
-				</tr>
-				<tr>
-				<td>Age
-				</td>
-				<td>Weight/Height
-				</td>
-				<td>Hair
-				</td>
-				<td>Eyes
-				</td>
-				</tr>
-				</table>
-				
-				<b style="font-size: 18pt;">Backstory</b>
-                  <ul style="font-size: 10pt;">
-				  <li class="sesparagraph">
-				  <img id="sesimg2" class="img" src="companylogo.png">
-				  <b>It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.</b></li>
-                  </ul>
 				  </div>
-				  <!-- Date 2 - End -->
-				  
-				  <!-- Date 3 -->				  
-				  <div id="sesdate3a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px;">
-				<tr>
-				<td>Race 3
-				</td>
-				<td>Class/Level
-				</td>
-				<td>Alignment
-				</td>
-				<td>Residence
-				</td>
-				</tr>
-				<tr>
-				<td>Occupation
-				</td>
-				<td>Date of Birth
-				</td>
-				<td>Nationality
-				</td>
-				<td>Gender
-				</td>
-				</tr>
-				<tr>
-				<td>Age
-				</td>
-				<td>Weight/Height
-				</td>
-				<td>Hair
-				</td>
-				<td>Eyes
-				</td>
-				</tr>
-				</table>
-				
-				<b style="font-size: 18pt;">Backstory</b>
-                  <ul style="font-size: 10pt;">
-				  <li class="sesparagraph">
-				  <img id="sesimg3" class="img" src="companylogo.png">
-				  <b>It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.</b></li>
-                  </ul>
+				  </fieldset>
 				  </div>
-				  <!-- Date 3 - End -->
-				  
-				  <!-- Date 4 -->				  
-				  <div id="sesdate4a" class="tab" style="display: none;">				
-				<table style="font-size: 12pt; width: 625px;">
-				<tr>
-				<td>Race 4
-				</td>
-				<td>Class/Level
-				</td>
-				<td>Alignment
-				</td>
-				<td>Residence
-				</td>
-				</tr>
-				<tr>
-				<td>Occupation
-				</td>
-				<td>Date of Birth
-				</td>
-				<td>Nationality
-				</td>
-				<td>Gender
-				</td>
-				</tr>
-				<tr>
-				<td>Age
-				</td>
-				<td>Weight/Height
-				</td>
-				<td>Hair
-				</td>
-				<td>Eyes
-				</td>
-				</tr>
-				</table>
-				
-				<b style="font-size: 18pt;">Backstory</b>
-                  <ul style="font-size: 10pt;">
-				  <li class="sesparagraph">
-				  <img id="sesimg4" class="img" src="companylogo.png">
-				  <b>It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.</b></li>
-                  </ul>
-				  </div>
-				  <!-- Date 4 - End -->
 				  
 
-</div>
-</div>
-</div>
 
 
 <script>
-$(document).ready(function() {	
-  $('.tabs2 .tab2-links a').on('click', function(e) {
-    var currentAttrValue = $(this).attr('href');
-    $('.tabs2 ' + currentAttrValue).show().siblings().hide();
-    $(this).parent('li').addClass('active').siblings().removeClass('active');
-    e.preventDefault();
-  });
-  
-  $('#chimg1').click(function(e) {
-		$('#chimg1a').toggle();
-	});
-	$('#chimg2').click(function(e) {
-		$('#chimg2a').toggle();
-	});
-	$('#chimg3').click(function(e) {
-		$('#chimg3a').toggle();
-	});
-	$('#chimg4').click(function(e) {
-		$('#chimg4a').toggle();
-	});
-	$('#chimg5').click(function(e) {
-		$('#chimg5a').toggle();
-	});
-	$('#chimg6').click(function(e) {
-		$('#chimg6a').toggle();
-	});
-	$('#chimg7').click(function(e) {
-		$('#chimg7a').toggle();
-	});
-	$('#chimg8').click(function(e) {
-		$('#chimg8a').toggle();
-	});
-	 $('.imgclose').click(function(e) {
-		$(this).parent('.imga').css('display', 'none');
-	});
+$(function() {
+      $('.majorpoints').click(function() {
+        $(this).find('.hiders').toggle();
+      });
+	  
 });
 
 </script>
@@ -282,48 +59,8 @@ $(document).ready(function() {
 	list-style: none;
 }
 
-.img {
-	float: right;
-	border: 1px solid;
-	width: 50px;
-	height: 50px;
-	cursor: pointer;
-}
-
-.imga {
-	position: absolute;
-    top: 0px;
-    left: 0px;
-    background: #00000099;
-    width: 100%;
-    height: auto;
-    z-index: 3;
-    text-align: center;
-    padding: 100px 0px;
-}
-
-.imgb {
-	width: 250px;
-	height: 250px;
-}
-
-.imgclose {
-	position: absolute;
-    top: 10px;
-    right: 10px;
-    color: #8fde39;
-    font-weight: bold;
-    font-size: 14pt;
-}
-
-.tab2-links:after {
-  display: inline-block;
-  clear: both;
-  content: '';
-}
-
-.tab2-links li {
-  position: relative;
+.majorpointslegend {
+	    position: relative;
     top: 0px;
     left: -25px;
     width: auto;
@@ -331,10 +68,5 @@ $(document).ready(function() {
     list-style: none;
     background: #0000004f;
 }
-.tab2-links li a {
-  text-decoration: none;
-  font-size: 18pt;
-  color: #8fde39;
-  padding: 5px;
-}
+
 </style>
