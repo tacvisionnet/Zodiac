6253b08b77920aa6e416f92439bb7c23748d7c87
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-04 00:11:26 -0700

    Add files via upload

--- items.php
+++ items.php
@@ -1,5 +1,16 @@
 <div id="itcontainer">
 
+<!-- chimages -->
+<div hidden id="itimg1a" class="itimga">
+<div class="itimgclose">X</div>
+<img id="itimg1b" class="itimgb" src="rolactid.png">
+</div>
+
+<div hidden id="itimg2a" class="itimga">
+<div class="itimgclose">X</div>
+<img id="itimg2b" class="itimgb" src="smartwatch.png">
+</div>
+
 
 <h1 style="font-family: 'Libre Barcode 128 Text', cursive; font-size: 92px; text-align: center; line-height: 12pt; font-weight: 400;">Items</h1>			
 		
@@ -14,8 +25,8 @@
 			</select>
    </div>
 				  
-				<div hidden id="item1" style="text-align: center;">
-				<img class="itemimg" src="rolactid.png">
+				<div hidden id="item1" style="text-align: center; margin: 50px 0px;">
+				<img id="itimg1" class="itimg" src="rolactid.png">
 				
 				<div class="itparagraph">
 				In my younger and more vulnerable years my father gave me some advice that I’ve been turning over in my mind ever since.
@@ -23,8 +34,8 @@
 				</div>
 </div>				
 
-<div hidden id="item2" style="text-align: center;">
-				<img class="itemimg" src="smartwatch.png">
+<div hidden id="item2" style="text-align: center; margin: 50px 0px;">
+<img id="itimg2" class="itimg" src="smartwatch.png">
 				
 				<div class="itparagraph">
 				In my younger and more vulnerable years my father gave me some advice that I’ve been turning over in my mind ever since.
@@ -49,6 +60,13 @@ $(function() {
         $("#item2").prop("hidden", true);
       }
     });
+	
+	$('#itimg1').click(function(e) {
+		$('#itimg1a').toggle();
+	});
+	$('#itimg2').click(function(e) {
+		$('#itimg2a').toggle();
+	});
 	  
 });
 
@@ -95,6 +113,39 @@ $(function() {
     background: #00000099;
 	font-size: 14pt;
 	color: #8edc39;
+}
+
+.itimg {
+	border: 1px solid;
+    height: 250px;
+	cursor: pointer;
+}
+
+
+.itimga {
+	position: absolute;
+    top: 0px;
+    left: 0px;
+    background: #00000099;
+    width: 100%;
+    height: auto;
+    z-index: 3;
+    text-align: center;
+    padding: 100px 0px;
+}
+
+.itimgb {
+	width: 250px;
+	height: 250px;
+}
+
+.itimgclose {
+	position: absolute;
+    top: 10px;
+    right: 10px;
+    color: #8fde39;
+    font-weight: bold;
+    font-size: 14pt;
 }
 
 </style>
