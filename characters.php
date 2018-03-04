91735dcd1ba382b1fab9c22510acb98d398f5c55
SheepishLamb <35158598+SheepishLamb@users.noreply.github.com>
2018-03-03 20:32:31 -0700

    Update characters.php

--- characters.php
+++ characters.php
@@ -184,17 +184,14 @@ $(document).ready(function() {
 		$('#chpcs1').css("display", "none");
 	
 	});
-});
-
-jQuery(document).ready(function() {
-  jQuery('.tabs .tab-links a').on('click', function(e) {
-    var currentAttrValue = jQuery(this).attr('href');
+  $('.tabs .tab-links a').on('click', function(e) {
+    var currentAttrValue = $(this).attr('href');
 
     // Show/Hide Tabs
-    jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+    $('.tabs ' + currentAttrValue).show().siblings().hide();
 
     // Change/remove current tab to active
-    jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+    $(this).parent('li').addClass('active').siblings().removeClass('active');
 
     e.preventDefault();
   });
